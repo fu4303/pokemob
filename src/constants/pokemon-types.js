eb890da3e856a53b7c42312690724398efbc287c
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-22 14:33:33 -0400

    create object of pokemon's types with color prop

---
+++ src/constants/pokemon-types.js
@@ -0,0 +1,57 @@
+export const POKEMON_TYPES = {
+    normal: {
+        color: '#5c5d5e',
+    },
+    fighting: {
+        color: '#c75600',
+    },
+    flying: {
+        color: '#009fc7',
+    },
+    poison: {
+        color: '#8100c7',
+    },
+    ground: {
+        color: '#80690f',
+    },
+    rock: {
+        color: '#666047',
+    },
+    bug: {
+        color: '#516647',
+    },
+    ghost: {
+        color: '#4f4f66',
+    },
+    steel: {
+        color: '#878787',
+    },
+    fire: {
+        color: '#a80000',
+    },
+    water: {
+        color: '#4287f5',
+    },
+    grass: {
+        color: '#267327',
+    },
+    electric: {
+        color: '#cfc800',
+    },
+    psychic: {
+        color: '#73006b',
+    },
+    ice: {
+        color: '#55a0a6',
+    },
+    dragon: {
+        color: '#574b7a',
+    },
+    fairy: {
+        color: '#8d6394',
+    },
+    dark: {
+        color: '#5c3d1c',
+    },
+
+}
