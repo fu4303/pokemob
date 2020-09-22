253d08664290accbb0ef9283426b4618d898bfe9
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-22 14:35:20 -0400

    create copy data component

---
+++ src/components/copy-data/copy-data.component.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+
+export const useStyles = makeStyles((theme) => ({
+  arrow: {
+    color: theme.palette.common.black,
+  },
+  tooltip: {
+    fontSize: "1rem",
+    backgroundColor: theme.palette.common.black,
+  },
+}));
+
+const CopyData = ({ children, onCopy }) => {
+  const toolTipClasses = useStyles();
+
+  const CustomTooltip = (props) => {
+    return <Tooltip arrow classes={toolTipClasses} {...props} />;
+  };
+
+  return (
+    <div className="container">
+      <CustomTooltip title="Copy" placement="top">
+        <IconButton aria-label="Copy" onClick={onCopy}>
+          <FileCopyIcon fontSize="small" color="primary" />
+        </IconButton>
+      </CustomTooltip>
+      {children}
+    </div>
+  );
+};
+
+export default CopyData;
