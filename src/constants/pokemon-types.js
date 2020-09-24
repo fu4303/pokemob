678dcbbcb1bc74096c6006a6124b276dd11c5e9e
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-24 17:54:57 -0400

    update poke types obj

--- src/constants/pokemon-types.js
+++ src/constants/pokemon-types.js
@@ -1,57 +1,74 @@
 export const POKEMON_TYPES = {
     normal: {
+        name: 'normal',
         color: '#5c5d5e',
     },
     fighting: {
+        name: 'fighting',
         color: '#c75600',
     },
     flying: {
+        name: 'flying',
         color: '#009fc7',
     },
     poison: {
+        name: 'poison',
         color: '#8100c7',
     },
     ground: {
+        name: 'ground',
         color: '#80690f',
     },
     rock: {
+        name: 'rock',
         color: '#666047',
     },
     bug: {
+        name: 'bug',
         color: '#516647',
     },
     ghost: {
+        name: 'ghost',
         color: '#4f4f66',
     },
     steel: {
+        name: 'steel',
         color: '#878787',
     },
     fire: {
+        name: 'fire',
         color: '#a80000',
     },
     water: {
+        name: 'water',
         color: '#4287f5',
     },
     grass: {
+        name: "grass",
         color: '#267327',
     },
     electric: {
+        name: 'electric',
         color: '#cfc800',
     },
     psychic: {
+        name: 'psychic',
         color: '#73006b',
     },
     ice: {
+        name: 'ice',
         color: '#55a0a6',
     },
     dragon: {
+        name: 'dragon',
         color: '#574b7a',
     },
     fairy: {
+        name: 'fairy',
         color: '#8d6394',
     },
     dark: {
+        name: 'dark',
         color: '#5c3d1c',
     },
-
 }
