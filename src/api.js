67ea0d0e5273c43eade439aea3ad038f9b771dec
Stanislawa Kutyepov <62475313+codemyjourney@users.noreply.github.com>
2020-09-25 14:55:37 -0400

    api function

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 export const fetchAPI = async (apiToFetch) => {
     const response = await fetch(apiToFetch);
-    const data = await response.json();
-    return data;
+    if (response.status >= 200 && response.status < 300) {
+        return await response.json();
+    } else {
+        const error = new Error(response.statusText || response.status)
+        error.response = response;
+        return response;
+    }
 } 
